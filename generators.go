dcc1180d3a6687d3347b8ca1c55e894b39f9a881
Brett Slatkin <brett@haxor.com>
2015-05-08 08:50:24 -0700

    Using string buffers makes the go code very easy to read

--- generators.go
+++ generators.go
@@ -12,44 +12,42 @@ type Word struct {
 	Text  string
 }
 
-func isLetter(r rune) bool {
+func isPartOfWord(r rune) bool {
 	return !(unicode.IsSpace(r) || unicode.IsPunct(r))
 }
 
 // ---
 
 func IndexWords(text string) (result []Word) {
-	var buf []rune
 	var word Word
 
-	checkFound := func() {
-		if len(buf) > 0 {
-			word.Text = string(buf)
-			result = append(result, word)
-			buf = make([]rune, 0)
-		}
-	}
-
 	for i, r := range text {
-		if isLetter(r) {
+		if isPartOfWord(r) {
 			// When the buffer was empty, but now we've found a
 			// rune that's part of a word, mark the index as the
 			// first character of a newly found word.
-			if len(buf) == 0 {
+			if len(word.Text) == 0 {
 				word.Index = i
 			}
-			// Append the rune to the current word's buffer.
-			buf = append(buf, r)
+			// Always append the rune to the current word.
+			word.Text += string(r)
 		} else {
-			// When the current rune is not part of a word, then
-			// we may have reached the end of the word.
-			checkFound()
+			// When the current rune is whitespace or punctuation,
+			// then we may have reached the end of the word and
+			// need to save a new result.
+			if len(word.Text) > 0 {
+				result = append(result, word)
+				word.Text = ""
+			}
 		}
 	}
 
-	// Any runes remaining in the buffer should be returned as part of a
-	// final found word.
-	checkFound()
+	// Any runes remaining in the buffer after we've gone through the text
+	// should be returned as part of a final found word.
+	if len(word.Text) > 0 {
+		result = append(result, word)
+		word.Text = ""
+	}
 
 	return
 }
@@ -94,7 +92,7 @@ func readUntil(targetStatus bool, reader *bufio.Reader) string {
 		if err != nil {
 			break
 		}
-		if isLetter(r) == targetStatus {
+		if isPartOfWord(r) == targetStatus {
 			reader.UnreadRune()
 			break
 		}
