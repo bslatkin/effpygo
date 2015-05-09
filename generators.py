ff73cfa14d7bf02869c3c28b21657fa720373a6e
Brett Slatkin <brett@haxor.com>
2015-05-09 10:01:07 -0700

    Removing errorless version

--- generators.py
+++ generators.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
+# Copyright 2015 Brett Slatkin, Pearson Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import csv
 import io
-
-
-# Show how exceptions propagate in Python at the call to next(it),
-# but in go you need to carry them through explicitly in Go.
 
 
 def load_csv_data(text):
@@ -17,13 +26,6 @@ def load_csv_data(text):
         point = float(row[0]), float(row[1])
         result.append(point)
     return result
-
-
-def load_csv_data_generator(text):
-    for row in csv.reader(io.StringIO(text)):
-        if len(row) != 2:
-            raise ValueError('Rows must have two entries')
-        yield float(row[0]), float(row[1])
 
 
 def load_csv_data_streaming(stream):
@@ -71,4 +73,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
