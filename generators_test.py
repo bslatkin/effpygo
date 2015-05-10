fe37f86413041446b89b9c7806da44254105fcbc
Brett Slatkin <brett@haxor.com>
2015-05-10 14:09:45 -0700

    Moving Python example into tests

---
+++ generators_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# Copyright 2015 Brett Slatkin, Pearson Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from generators import *
+
+
+data = '1.0,2.5\n3.5,4.1\n7.5,2.2\n6.9,1.1\n'
+
+
+class Test(unittest.TestCase):
+
+    def testSingleLoad(self):
+        try:
+            rows = load_csv_data(io.StringIO(data))
+        except (ValueError, IOError):
+            raise Exception('Broke reading CSV')
+
+        for i, row in enumerate(rows):
+            print('Row %d is %r' % (i, row))
+
+    def testStreaming(self):
+        stream = io.StringIO(data)
+        it = load_csv_data_streaming(stream)
+        try:
+            for i, row in enumerate(it):
+                print('Row %d is %r' % (i, row))
+        except (ValueError, IOError):
+            raise Exception('Broke reading CSV')
+
+    def testExplicitLooping(self):
+        stream = io.StringIO(data)
+        it = enumerate(load_csv_data_streaming(stream))
+        while True:
+            try:
+                i, row = next(it)
+            except StopIteration:
+                break
+            except (ValueError, IOError):
+                raise Exception('Broke after row')
+            else:
+                print('Row %d is %r' % (i, row))
+
+    def testDistance(self):
+        stream = io.StringIO(data)
+        it = load_csv_data_streaming(stream)
+        for i, distance in enumerate(distance_stream(it)):
+            print('Move %d was %f far' % (i, distance))
+
+
+if __name__ == '__main__':
+    unittest.main()
